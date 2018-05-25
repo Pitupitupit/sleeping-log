af313dab9252bdf6b4493a65b2d1e8260f398508
Pitu <piotrekgrochalski@gmail.com>
2018-05-25 19:29:05 +0200

    wow

--- app/src/main/java/log/sleeping/android/arcturuspiotrek/sleepinglog/StatisticsActivity.java
+++ app/src/main/java/log/sleeping/android/arcturuspiotrek/sleepinglog/StatisticsActivity.java
@@ -1,6 +1,78 @@
 package log.sleeping.android.arcturuspiotrek.sleepinglog;
 
+import android.arch.persistence.room.Room;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Date;
+
+import log.sleeping.android.arcturuspiotrek.sleepinglog.db.AppDatabase;
+import log.sleeping.android.arcturuspiotrek.sleepinglog.entities.Sleep;
+import log.sleeping.android.arcturuspiotrek.sleepinglog.entities.User;
 
 public class StatisticsActivity extends AppCompatActivity {
+    int userId;
+    User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            userId = extras.getInt("userId");
+
+        } else {
+            userId = -1; //bląd
+        }
+
+        final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").allowMainThreadQueries().build();
+        user = db.userDao().getUserById(userId);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        //graph.getViewport().setScalable(true);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
